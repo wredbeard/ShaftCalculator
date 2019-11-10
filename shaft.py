c27397ed309e952796735d564faf250bc589e9b3
wredbeard <42924081+wredbeard@users.noreply.github.com>
2019-11-09 18:12:13 -0600

    added user input functions

--- shaft.py
+++ shaft.py
@@ -3,31 +3,42 @@ import baseclasses
 
 shaft = baseclasses.shaft()
 
-# Define cylinder 1
-cylinder1 = baseclasses.cylinder(5, 10.25)
-shaft.add_feature(cylinder1)
 
-# Define cylinder 2
-cylinder2 = baseclasses.cylinder(7, 18)
-shaft.add_feature(cylinder2)
+def _input(message,in_type=float):
+    while True:
+      try:
+        return in_type (input(message))
+      except:
+        shaft_input()
 
-# Define cylinder 3
-cylinder3 = baseclasses.cylinder()
-cylinder3.diameter = 4.25
-cylinder3.length = 7
 
-shaft.add_feature(cylinder3)
+def shaft_input():
+    in_diameter = _input("Please enter cylinder diameter: ", float)
+    in_length = _input("Please enter cylinder length: ", float)
+    cylinder = baseclasses.cylinder(in_diameter, in_length)
+    shaft.add_feature(cylinder)
+    in_continue = input("Would you like to enter another cylinder? (y/n): ")
+    if in_continue == "y":
+        shaft_input()
+    else:
+        display_results()
 
-count = 0 
-
-for feature in shaft.features:
-    count = count + 1
-    print("Cylinder " + str(count) + " => " + "Dia: " + str(feature.diameter) + " Length: " + str(feature.length))
-    print("  volume = ", feature.get_volume())
 
 #-- Display some info about the Shaft, which are derived from the Cylinder objects
 #-- of which it is made of.
-print("\nOverall Length: %s" % shaft.get_length())
-print("\nVolume: %f cubic units" % shaft.get_volume())
-print("\nWeight: %f (lbs): " % shaft.weight)
+def display_results():
+    count = 0
+
+    for feature in shaft.features:
+        count = count + 1
+        print("Cylinder " + str(count) + " => " + "Dia: " + str(feature.diameter) + " Length: " + str(feature.length))
+        print("  volume = ", feature.get_volume())
+
+    print("\nTotal Volume: %f cubic units" % shaft.get_volume())
+    print("\nOverall Length: %s" % shaft.get_length())
+    print("\nWeight: %f (lbs): " % shaft.weight)
+
+
+if __name__ == '__main__':
+    shaft_input()
 
